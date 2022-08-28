938af31f961044c976fdcc937fd040b1060e6758
Edu <particularspam@gmail.com>
2022-08-27 19:10:13 -0500

    Update attack button width

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -164,14 +164,17 @@ function secuenciaAtaque() {
         ataqueJugador.push("FUEGO");
         console.log(ataqueJugador);
         boton.style.background = "#112f58";
+        boton.disabled = true;
       } else if (e.target.textContent === "💧") {
         ataqueJugador.push("AGUA");
         console.log(ataqueJugador);
         boton.style.background = "#112f58";
+        boton.disabled = true;
       } else {
         ataqueJugador.push("TIERRA");
         console.log(ataqueJugador);
         boton.style.background = "#112f58";
+        boton.disabled = true;
       }
       ataqueAleatorioEnemigo();
     });
@@ -263,9 +266,6 @@ function crearMensaje(resultado) {
 }
 function crearMensajeFinal(resultadoFinal) {
   sectionMensajes.innerHTML = resultadoFinal;
-  botonFuego.disabled = true;
-  botonAgua.disabled = true;
-  botonTierra.disabled = true;
   sectionReiniciar.style.display = "block";
 }
 function reiniciarJuego() {
