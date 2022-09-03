e6f138cc5ee35afb540e10741b2932ec79ce373b
Edu <particularspam@gmail.com>
2022-09-03 03:30:59 -0500

    Add Screen Rendering Optimization

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -21,6 +21,7 @@ const mapa = document.getElementById("mapa");
 
 let jugadorId = null;
 let mokepones = [];
+let mokeponesEnemigos = [];
 let ataqueJugador = [];
 let ataqueEnemigo = [];
 let opcionDeMokepones;
@@ -350,9 +351,9 @@ function pintarCanvas() {
 
   enviarPosicion(mascotaJugadorObjeto.x, mascotaJugadorObjeto.y);
 
-  hipodogeEnemigo.pintarMokepon();
-  capipepoEnemigo.pintarMokepon();
-  ratigueyaEnemigo.pintarMokepon();
+  mokeponesEnemigos.forEach(function (mokepon) {
+    mokepon.pintarMokepon();
+  });
   if (
     mascotaJugadorObjeto.velocidadX !== 0 ||
     mascotaJugadorObjeto.velocidadY !== 0
@@ -426,7 +427,7 @@ function enviarPosicion(x, y) {
     if (res.ok) {
       res.json().then(function ({ enemigos }) {
         console.log(enemigos);
-        enemigos.forEach(function (enemigo) {
+        mokeponesEnemigos = enemigos.map(function (enemigo) {
           let mokeponEnemigo = null;
           const mokeponNombre = enemigo.mokepon.nombre || "";
           if (mokeponNombre === "Hipodoge") {
@@ -455,7 +456,7 @@ function enviarPosicion(x, y) {
           mokeponEnemigo.x = enemigo.x;
           mokeponEnemigo.y = enemigo.y;
 
-          mokeponEnemigo.pintarMokepon();
+          return mokeponEnemigo;
         });
       });
     }
